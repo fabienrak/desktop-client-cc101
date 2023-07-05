491fdbf0953dddb89dec1c94368dc833daddc4ce
DEVJIJASWATI <DEV.fabmia@gmail.com>
2023-07-05 10:09:12 +0300

    chore: launch app

--- src/main/java/org/app/App.java
+++ src/main/java/org/app/App.java
@@ -11,17 +11,12 @@ import java.sql.Connection;
 public class App extends Application {
 
     public static void main(String[] args) {
-        System.setProperty("javafx.preloader", AppPreloader.class.getCanonicalName());
         launch(args);
     }
 
     @Override
-    public void init() throws Exception {
-        Connection connection = DatabaseConnection.getConnection();
-    }
-
-    @Override
     public void start(Stage stage) throws Exception {
+        Connection connection = DatabaseConnection.getConnection();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/auth/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(this.getClass().getResource("/css/style.css").toExternalForm());
